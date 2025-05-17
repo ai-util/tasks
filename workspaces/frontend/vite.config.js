ca29591a34dfd9ee042a021d71a58371efcff444
Christoph Herrmann <github@sharaal.de>
2025-05-17 22:51:36 +0200

    Prepare NPM workspace for backend

---
+++ workspaces/frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  root: path.resolve(__dirname),
+  publicDir: path.resolve(__dirname, '../tasks'),
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      },
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  }
+}) 
